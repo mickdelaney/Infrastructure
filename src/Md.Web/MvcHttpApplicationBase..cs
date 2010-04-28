e301966c2b76f0d81c8dfa206bb5921390345408
mickdelaney <mickdelaney@gmail.com>
2010-04-28 20:37:42 +0100

    Added Infrastructure Specs

---
+++ src/Md.Web/MvcHttpApplicationBase..cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using Castle.Windsor;
+using log4net;
+using NServiceBus;
+
+namespace Md.Web
+{
+    public class MvcHttpApplicationBase<T> : HttpApplication, IContainerAccessor where T : class 
+    {
+        protected readonly ILog Logger = LogManager.GetLogger(typeof(T));
+
+        protected static WindsorContainer _container;
+
+        public static IWindsorContainer Container
+        {
+            get { return _container; }
+        }
+
+        IWindsorContainer IContainerAccessor.Container
+        {
+            get { return Container; }
+        }
+
+        public static IBus Bus { get; private set; }
+
+    }
+}
